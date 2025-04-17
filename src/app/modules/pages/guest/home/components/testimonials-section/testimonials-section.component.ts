ee6f9361fd682e3edc6468105465a014c26dfdaa
Mashhurakhon <mashhurakhon03@gmail.com>
2025-04-17 13:27:17 +0500

    testimonials

--- src/app/modules/pages/guest/home/components/testimonials-section/testimonials-section.component.ts
+++ src/app/modules/pages/guest/home/components/testimonials-section/testimonials-section.component.ts
@@ -1,12 +1,15 @@
 import { Component } from '@angular/core';
+import SwiperComponent  from 'swiper';
 
 @Component({
   selector: 'app-testimonials-section',
   standalone: true,
-  imports: [],
+  imports: [SwiperComponent],
   templateUrl: './testimonials-section.component.html',
   styleUrl: './testimonials-section.component.scss'
 })
-export class TestimonialsSectionComponent {
+export class TestimonialsSectionComponent{
+
+  
 
 }
